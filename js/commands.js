ccff944d8dd8c810f9bcc08006b39ce4d44b9f73
1hachem <hachem.betrouni@g.enp.edu.dz>
2022-08-12 00:51:41 +0100

    init

---
+++ js/commands.js
@@ -0,0 +1,71 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = 'mailto:forrest@fkcodes.com';
+
+about = [
+  "<br>",
+  "Hi there 👋",
+  "my name is Hachem Betrouni, I am a Data Science and AI student from Algeria ✨",
+  "🔭 I’m currently working at <a href=\"https://www.instadeep.com/\">InstaDeep</a> on predicting and generating new variants of Sars-CoV-2 using GANs", 
+  "🌱 looking forward to joining <a href=\"https://www.deepmind.com/\">DeepMind</a> as an AI research intern in the near future",
+  "🤓 I freelance on <a href=\"https://www.upwork.com/freelancers/~01b79e4e53cd1ca324\">Upwork</a> in machine learning and data science", 
+  "📫 Reach me at: hachem.betrouni@g.enp.edu.dz",
+  "🤫 Fun fact about me : I never owned a smartphone 📱",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          Who am I ?',
+  '<span class="command">whoami</span>         Who are you ?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">ForrestKnight (FK) Not A Corporation. All knights reserved.</span>',
+'', '                                                                                                                                                      ', '        ,--,                                                                                                                                          ', "      ,--.'|                       ,---,                       ____                     ____                                    ,--,                  ", "   ,--,  | :                     ,--.' |                     ,'  , `.                 ,'  , `.  ,--,                          ,--.'|                  ", ",---.'|  : '                     |  |  :                  ,-+-,.' _ |    ,---,.    ,-+-,.' _ |,--.'|         ,---,            |  | :                  ", "|   | : _' |                     :  :  :               ,-+-. ;   , ||  ,'  .' | ,-+-. ;   , |||  |,      ,-+-. /  |           :  : '                  ", ":   : |.'  |  ,--.--.     ,---.  :  |  |,--.   ,---.  ,--.'|'   |  ||,---.'   ,,--.'|'   |  ||`--'_     ,--.'|'   |  ,--.--.  |  ' |                  ", '|   \' \'  ; : /       \\   /     \\ |  :  \'   |  /     \\|   |  ,\', |  |,|   |    |   |  ,\', |  |,,\' ,\'|   |   |  ,"\' | /       \\ \'  | |                  ', "'   |  .'. |.--.  .-. | /    / ' |  |   /' : /    /  |   | /  | |--' :   :  .'|   | /  | |--' '  | |   |   | /  | |.--.  .-. ||  | :                  ", "|   | :  | ' \\__\\/: . ..    ' /  '  :  | | |.    ' / |   : |  | ,    :   |.'  |   : |  | ,    |  | :   |   | |  | | \\__\\/: . .'  : |__                ", '\'   : |  : ; ," .--.; |\'   ; :__ |  |  \' | :\'   ;   /|   : |  |/     `---\'    |   : |  |/     \'  : |__ |   | |  |/  ," .--.; ||  | \'.\'|               ', "|   | '  ,/ /  /  ,.  |'   | '.'||  :  :_:,''   |  / |   | |`-'               |   | |`-'      |  | '.'||   | |--'  /  /  ,.  |;  :    ;               ", ";   : ;--' ;  :   .'   \\   :    :|  | ,'    |   :    |   ;/                   |   ;/          ;  :    ;|   |/     ;  :   .'   \\  ,   /                ", "|   ,/     |  ,     .-./\\   \\  / `--''       \\   \\  /'---'                    '---'           |  ,   / '---'      |  ,     .-./---`-'                 ", "'---'       `--`---'     `----'               `----'                                           ---`-'              `--`---'                           ", '                                                                                                                                                      ', '',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
