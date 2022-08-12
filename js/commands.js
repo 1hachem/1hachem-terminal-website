3b933c03d531569781cc42441bd6cd15b448f75b
1hachem <hachem.betrouni@g.enp.edu.dz>
2022-08-12 12:17:47 +0100

    phone friendly

--- js/commands.js
+++ js/commands.js
@@ -4,22 +4,12 @@ var github = "https://github.com/1hachem/";
 var email = "mailto:hachem.betrouni@g.enp.edu.dz";
 var cv = "documents/cv.pdf";
 
-about = [
-  "<br>",
-  "Hi there 👋",
-  "my name is Hachem Betrouni, I am a Data Science and AI student from Algeria ✨",
-  "🔭 I’m currently working at <a href=\"https://www.instadeep.com/\">InstaDeep</a> on predicting and generating new variants of Sars-CoV-2 using GANs", 
-  "🌱 looking forward to joining <a href=\"https://www.deepmind.com/\">DeepMind</a> as an AI research intern in the near future",
-  "🤓 I freelance on <a href=\"https://www.upwork.com/freelancers/~01b79e4e53cd1ca324\">Upwork</a> in machine learning and data science", 
-  "📫 Reach me at: hachem.betrouni@g.enp.edu.dz",
-  "🤫 Fun fact about me : I never owned a smartphone 📱",
-  "<br>"
-];
-
 social = [
   "<br>",
   'linkedin      <a href="' + linkedin + '" target="_blank">in/hachem-betrouni' + '</a>',
+  "<br>",
   'github        <a href="' + github + '" target="_blank">github/1hachemt' + "</a>",
+  "<br>",
   'upwrok        <a href="' + upwork + '" target="_blank">upwork/Hachem Betrouni' + "</a>",
   "<br>"
 ];
@@ -42,16 +32,67 @@ help = [
   '<span class="command">cv</span>             Curriculum vitae',
   '<span class="command">social</span>         Display social networks',
   '<span class="command">projects</span>       View coding projects',
-  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">help</span>           You know what this does',
   '<span class="command">email</span>          Email me',
   '<span class="command">clear</span>          Clear terminal',
   '<span class="command">banner</span>         Display the header',
   "<br>",
 ];
 
-banner = [
-'', '                                                                                                                                                      ', '        ,--,                                                                                                                                          ', "      ,--.'|                       ,---,                       ____                     ____                                    ,--,                  ", "   ,--,  | :                     ,--.' |                     ,'  , `.                 ,'  , `.  ,--,                          ,--.'|                  ", ",---.'|  : '                     |  |  :                  ,-+-,.' _ |    ,---,.    ,-+-,.' _ |,--.'|         ,---,            |  | :                  ", "|   | : _' |                     :  :  :               ,-+-. ;   , ||  ,'  .' | ,-+-. ;   , |||  |,      ,-+-. /  |           :  : '                  ", ":   : |.'  |  ,--.--.     ,---.  :  |  |,--.   ,---.  ,--.'|'   |  ||,---.'   ,,--.'|'   |  ||`--'_     ,--.'|'   |  ,--.--.  |  ' |                  ", '|   \' \'  ; : /       \\   /     \\ |  :  \'   |  /     \\|   |  ,\', |  |,|   |    |   |  ,\', |  |,,\' ,\'|   |   |  ,"\' | /       \\ \'  | |                  ', "'   |  .'. |.--.  .-. | /    / ' |  |   /' : /    /  |   | /  | |--' :   :  .'|   | /  | |--' '  | |   |   | /  | |.--.  .-. ||  | :                  ", "|   | :  | ' \\__\\/: . ..    ' /  '  :  | | |.    ' / |   : |  | ,    :   |.'  |   : |  | ,    |  | :   |   | |  | | \\__\\/: . .'  : |__                ", '\'   : |  : ; ," .--.; |\'   ; :__ |  |  \' | :\'   ;   /|   : |  |/     `---\'    |   : |  |/     \'  : |__ |   | |  |/  ," .--.; ||  | \'.\'|               ', "|   | '  ,/ /  /  ,.  |'   | '.'||  :  :_:,''   |  / |   | |`-'               |   | |`-'      |  | '.'||   | |--'  /  /  ,.  |;  :    ;               ", ";   : ;--' ;  :   .'   \\   :    :|  | ,'    |   :    |   ;/                   |   ;/          ;  :    ;|   |/     ;  :   .'   \\  ,   /                ", "|   ,/     |  ,     .-./\\   \\  / `--''       \\   \\  /'---'                    '---'           |  ,   / '---'      |  ,     .-./---`-'                 ", "'---'       `--`---'     `----'               `----'                                           ---`-'              `--`---'                           ", '                                                                                                                                                      ', '',
-  '<span class="color2">Welcome to my interactive web terminal.</span>',
-  "For a list of available commands, type <span class=\"command\">help</span>",
-];
+const deviceTypeBanner = () => {
+  const ua = navigator.userAgent;
+  
+  if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+
+      banner = ['<span class="color2">Welcome to my interactive web terminal.</span>',
+      "For a list of available commands, type <span class=\"command\">help</span>"];
+
+  }else{
+
+  banner = [
+    '', '                                                                                                                                                      ', '        ,--,                                                                                                                                          ', "      ,--.'|                       ,---,                       ____                     ____                                    ,--,                  ", "   ,--,  | :                     ,--.' |                     ,'  , `.                 ,'  , `.  ,--,                          ,--.'|                  ", ",---.'|  : '                     |  |  :                  ,-+-,.' _ |    ,---,.    ,-+-,.' _ |,--.'|         ,---,            |  | :                  ", "|   | : _' |                     :  :  :               ,-+-. ;   , ||  ,'  .' | ,-+-. ;   , |||  |,      ,-+-. /  |           :  : '                  ", ":   : |.'  |  ,--.--.     ,---.  :  |  |,--.   ,---.  ,--.'|'   |  ||,---.'   ,,--.'|'   |  ||`--'_     ,--.'|'   |  ,--.--.  |  ' |                  ", '|   \' \'  ; : /       \\   /     \\ |  :  \'   |  /     \\|   |  ,\', |  |,|   |    |   |  ,\', |  |,,\' ,\'|   |   |  ,"\' | /       \\ \'  | |                  ', "'   |  .'. |.--.  .-. | /    / ' |  |   /' : /    /  |   | /  | |--' :   :  .'|   | /  | |--' '  | |   |   | /  | |.--.  .-. ||  | :                  ", "|   | :  | ' \\__\\/: . ..    ' /  '  :  | | |.    ' / |   : |  | ,    :   |.'  |   : |  | ,    |  | :   |   | |  | | \\__\\/: . .'  : |__                ", '\'   : |  : ; ," .--.; |\'   ; :__ |  |  \' | :\'   ;   /|   : |  |/     `---\'    |   : |  |/     \'  : |__ |   | |  |/  ," .--.; ||  | \'.\'|               ', "|   | '  ,/ /  /  ,.  |'   | '.'||  :  :_:,''   |  / |   | |`-'               |   | |`-'      |  | '.'||   | |--'  /  /  ,.  |;  :    ;               ", ";   : ;--' ;  :   .'   \\   :    :|  | ,'    |   :    |   ;/                   |   ;/          ;  :    ;|   |/     ;  :   .'   \\  ,   /                ", "|   ,/     |  ,     .-./\\   \\  / `--''       \\   \\  /'---'                    '---'           |  ,   / '---'      |  ,     .-./---`-'                 ", "'---'       `--`---'     `----'               `----'                                           ---`-'              `--`---'                           ", '                                                                                                                                                      ', '',
+      '<span class="color2">Welcome to my interactive web terminal.</span>',
+      "For a list of available commands, type <span class=\"command\">help</span>",
+    ];
+  }
+  
+  return banner;
+};
+
+const deviceTypeAbout = () => {
+  const ua = navigator.userAgent;
+  
+  if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+    about = [
+      "<br>",
+      "Hi there 👋",
+      "my name is Hachem Betrouni,", "I am a Data Science and AI student from Algeria ✨","<br>",
+      "🔭 I’m currently working at <a href=\"https://www.instadeep.com/\">InstaDeep</a>","on predicting and generating","new variants of Sars-CoV-2 using GANs","<br>",
+      "🌱 looking forward to joining <a href=\"https://www.deepmind.com/\">DeepMind</a>","as an AI research intern in the near future","<br>",
+      "🤓 I freelance on <a href=\"https://www.upwork.com/freelancers/~01b79e4e53cd1ca324\">Upwork</a>","in machine learning and data science","<br>", 
+      "📫 Reach me at: hachem.betrouni@g.enp.edu.dz","<br>",
+      "🤫 Fun fact about me : I never owned a smartphone 📱",
+      "<br>"
+    ];
+
+  }else{
+  
+    about = [
+      "<br>",
+      "Hi there 👋",
+      "my name is Hachem Betrouni, I am a Data Science and AI student from Algeria ✨",
+      "🔭 I’m currently working at <a href=\"https://www.instadeep.com/\">InstaDeep</a> on predicting and generating new variants of Sars-CoV-2 using GANs", 
+      "🌱 looking forward to joining <a href=\"https://www.deepmind.com/\">DeepMind</a> as an AI research intern in the near future",
+      "🤓 I freelance on <a href=\"https://www.upwork.com/freelancers/~01b79e4e53cd1ca324\">Upwork</a> in machine learning and data science", 
+      "📫 Reach me at: hachem.betrouni@g.enp.edu.dz",
+      "🤫 Fun fact about me : I never owned a smartphone 📱",
+      "<br>"
+    ];
+  }
+  return about;
+};
+
+
+banner = deviceTypeBanner();
+about = deviceTypeAbout();
 
